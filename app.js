407475ed9d5f600587f6512c2b32266a8fdb9f48
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-12-01 11:15:06 +0530

    create table query inserted

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mysql = require('mysql');
 const app = express();
 const port = 4000;
 
+var spbtable = "spbtable";
+
 const connection = mysql.createConnection({
     host:"testdb.cergfpiaziwa.us-east-1.rds.amazonaws.com",
     user:"admin",
@@ -28,6 +30,13 @@ app.get('/',(req, res)=>{
         if(err) throw err;
         console.log("DATABASE create query run");
     });
+
+    let sql2 ="CREATE TABLE IF NOT EXISTS ?? (ID int AUTO_INCREMENT NOT NULL,patientname VARCHAR(255),caretaker VARCHAR(255),caretakernum int(11),start_date timestamp,schedulestatus , mybool boolean not null default 0 ,PRIMARY KEY(ID))";
+    let tablestarterquery = connection.query(sql2,[spbtable],(err,rows)=>{
+        if(err) throw err;
+        console.log("create table query run");
+    });
+
 });
 
 app.listen(port,()=>{
