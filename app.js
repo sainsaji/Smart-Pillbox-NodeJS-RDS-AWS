22f85833ce500408906255eaf063116e5a7ee392
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-12-01 11:25:50 +0530

    corrected create table error

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/',(req, res)=>{
         console.log("DATABASE create query run");
     });
 
-    let sql2 ="CREATE TABLE IF NOT EXISTS ?? (ID int AUTO_INCREMENT NOT NULL,patientname VARCHAR(255),caretaker VARCHAR(255),caretakernum int(11),start_date timestamp,schedulestatus , mybool boolean not null default 0 ,PRIMARY KEY(ID))";
+    let sql2 ="CREATE TABLE IF NOT EXISTS ?? (ID int AUTO_INCREMENT NOT NULL,patientname VARCHAR(255),caretaker VARCHAR(255),caretakernum int(11),start_date timestamp,schedulestatus boolean not null default 0 ,PRIMARY KEY(ID))";
     let tablestarterquery = connection.query(sql2,[spbtable],(err,rows)=>{
         if(err) throw err;
         console.log("create table query run");
