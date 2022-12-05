3ca69f8d9e8f9100e08e87cd4012a0760abc47e0
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-12-06 00:30:34 +0530

    added table truncate and display table

--- routes/calenderRoute.js
+++ routes/calenderRoute.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const route = express.Router();
+const dbconfig = require('../config/db-config');
+const mysql = require('mysql');
+var spbtable = "spbtable";
+
+const connection = mysql.createConnection({
+    host:dbconfig.host,
+    user:dbconfig.user,
+    password:dbconfig.password,
+    database:dbconfig.database,
+    port:dbconfig.port,
+    connectionLimit: dbconfig.connectionLimit,
+    queueLimit: dbconfig.queueLimit,
+    acquireTimeout: dbconfig.acquireTimeout
+});
 
 route.get('/calendar', (req, res)=>{
+    let sql = "SELECT * FROM ??";
+    let query = connection.query(sql,[spbtable],(err,rows,res1)=>{
+        if(err) throw err;
     res.render('pages/calendar',{
-        title:"Calendar"
+        title:"Schedule List",
+        schedulelist : rows
     });
+});
+});
+
+route.get('/clearSchedule', (req, res)=>{
+    let sql = "TRUNCATE TABLE ??;";
+    let query = connection.query(sql,[spbtable],(err,rows,res1)=>{
+        if(err) throw err;
+    res.render('pages/calendar',{
+        title:"Schedule List",
+        schedulelist : rows
+    });
+    res.redirect('back');
+});
 });
 module.exports = route;
